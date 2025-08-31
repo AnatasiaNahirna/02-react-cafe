bfc5c81bd4d42e47fcec661bac39fe80048bc03f
Anastasia <akroyl419@gmail.com>
2025-08-31 22:09:42 +0300

    start

---
+++ src/components/VoteStats/VoteStats.tsx
@@ -0,0 +1,22 @@
+import css from "./VoteStats.module.css"
+
+interface VoteStatsProps {
+    votes: {
+        good: number;
+        neutral: number;
+        bad: number;
+    };
+    totalVotes: number;
+    positiveRate: number;
+}
+
+export default function VoteStats(props: VoteStatsProps) {
+  return  <div className={css.container}>
+  <p className={css.stat}>Good: <strong>{props.votes.good}</strong></p>
+      <p className={css.stat}>Neutral: <strong>{props.votes.neutral}</strong></p>
+      <p className={css.stat}>Bad: <strong>{props.votes.bad}</strong></p>
+      <p className={css.stat}>Total: <strong>{props.totalVotes}</strong></p>
+      <p className={css.stat}>Positive: <strong>{props.positiveRate}%</strong></p>
+</div>
+
+}
