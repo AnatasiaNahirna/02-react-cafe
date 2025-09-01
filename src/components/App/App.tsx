5643c4937bd12ac210038e7a34eecf262c7c0f7d
Anastasia <akroyl419@gmail.com>
2025-09-01 14:23:52 +0300

    fixfile2

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,5 @@
 import css from './App.module.css'
-import Cafeinfo from '../CafeInfo/c'
+import Cafeinfo from '../CafeInfo/CafeInfo'
 import { useState } from 'react'
 import type { Votes, VoteType } from '../../types/votes'
 import VoteOptions from '../VoteOptions/VoteOptions'
